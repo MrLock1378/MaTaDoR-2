a0ba11161f66e68814be1610325e83e11770b1bc
Mohammadxx3 <hasanohamadi26@gmail.com>
2017-05-01 09:33:00 +0430

    Update ping2.lua

--- plugins/ping2.lua
+++ plugins/ping2.lua
@@ -1,7 +1,7 @@
 
 local function run(msg, matches)
 if matches[1] == 'ping' then
-   tdcli.sendDocument(msg.chat_id_, msg.id_, 0, 1, nil, './bot/ping.mp4', 'Online..!', dl_cb, nil)
+   tdcli.sendDocument(msg.chat_id_, msg.id_, 0, 1, nil, './bot/ping.mp4','💥pong! spłÐΞЯ Is Online :)💥➖➖➖🔥ℓσcкєятєαм🔥', dl_cb, nil)
 end
     end
 
